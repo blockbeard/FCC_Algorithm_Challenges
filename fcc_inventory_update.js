f1471b89a810e93729fde41a1c8e869bfc32918d
Chris Wilson <chris2097@gmail.com>
2016-07-07 15:14:12 +0100

    Inventory update is working except my sort is busted.

--- fcc_inventory_update.js
+++ fcc_inventory_update.js
@@ -4,23 +4,31 @@
 
 function updateInventory(arr1, arr2) {
     // All inventory must be accounted for or you're fired!
-    
+    // iterate through arr2
+    for (var i = 0; i < arr2.length; i++){
+        var present = false;
+
+        // iterate through arr1
+        for (var n = 0; n < arr1.length; n++){
+            // if item is already in arr1
+            if (arr1[n][1] === arr2[i][1]){
+                // note presence
+                present = true;
+                // add to inventory
+                arr1[n][0] = arr1[n][0] + arr2[i][0];
+            }
+        }
+        // if not added to arr1 already
+        if (!present){
+            arr1.push(arr2[i]);
+        }
+    }
+    // sort arr1 by alphabetical
+    arr1.sort(function(a,b){
+        var x = a[1].toLowerCase,
+            y = b[1].toLowerCase;
+        return x-y});
+    console.log(arr1);
     return arr1;
 }
-
-// Example inventory lists
-var curInv = [
-    [21, "Bowling Ball"],
-    [2, "Dirty Sock"],
-    [1, "Hair Pin"],
-    [5, "Microphone"]
-];
-
-var newInv = [
-    [2, "Hair Pin"],
-    [3, "Half-Eaten Apple"],
-    [67, "Bowling Ball"],
-    [7, "Toothpaste"]
-];
-
-updateInventory(curInv, newInv);
+updateInventory([[0, "Bowling Ball"], [0, "Dirty Sock"], [0, "Hair Pin"], [0, "Microphone"]], [[1, "Hair Pin"], [1, "Half-Eaten Apple"], [1, "Bowling Ball"], [1, "Toothpaste"]]);
