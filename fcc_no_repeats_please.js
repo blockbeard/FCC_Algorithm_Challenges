661868387ec445e51b0c2795a94f1095ad838add
Chris Wilson <chris2097@gmail.com>
2016-07-11 20:45:58 +0100

    Completed challenge. Global variables were fucking up the automated testing. Global variables are bad M'kay.

--- fcc_no_repeats_please.js
+++ fcc_no_repeats_please.js
@@ -2,44 +2,44 @@
  * Created by chris_000 on 07/07/2016.
  */
 
-var permutations = [],
-    filteredPermutations = [];
-
-function permutationArr(num)
-{
-    var arr = (num + '').split('');
-
-    function swap(a, b)
-    {
-        var tmp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = tmp;
-    }
-
-    function generate(n) {
-        if (n == 1) {
-            permutations.push(arr.join(""));
-        } else {
-            for (var i = 0; i != n; ++i) {
-                generate(n - 1);
-                swap(n % 2 ? 0 : i, n - 1);
-            }
-        }
-    }
-
-    generate(arr.length);
-    console.log(permutations);
-}
-function duplicates(str){
-    var dupes = new RegExp(/(.)\1/);
-    return (!dupes.test(str));
-}
-
-function killRepeats(){
-    filteredPermutations = permutations.filter(duplicates);
-}
 
 function permAlone(str) {
+    var permutations = [],
+        filteredPermutations = [];
+    function permutationArr(num)
+    {
+        var arr = (num + '').split('');
+
+        function swap(a, b)
+        {
+            var tmp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = tmp;
+        }
+
+        function generate(n) {
+            if (n == 1) {
+                permutations.push(arr.join(""));
+            } else {
+                for (var i = 0; i != n; ++i) {
+                    generate(n - 1);
+                    swap(n % 2 ? 0 : i, n - 1);
+                }
+            }
+        }
+
+        generate(arr.length);
+        console.log(permutations);
+    }
+    function duplicates(str){
+        var dupes = new RegExp(/(.)\1/);
+        return (!dupes.test(str));
+    }
+
+    function killRepeats(){
+        filteredPermutations = permutations.filter(duplicates);
+    }
+
     permutationArr(str);
     killRepeats();
     console.log(filteredPermutations);
