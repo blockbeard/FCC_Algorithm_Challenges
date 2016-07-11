18f14586d97970d06c23fcea3faa996835faa856
Chris Wilson <chris2097@gmail.com>
2016-07-11 21:02:06 +0100

    Started Friendly Date Ranges

---
+++ fcc_friendly_date_ranges.js
@@ -0,0 +1,16 @@
+/**
+ * Created by chris_000 on 11/07/2016.
+ */
+
+function makeFriendlyDates(arr) {
+    var start = new Date(arr[0]),
+        finish = new Date(arr[1]),
+        friendlyStart,
+        friendlyEnd,
+        friendlyDate = [];
+
+    return friendlyDate;
+}
+
+makeFriendlyDates(['2016-07-01', '2016-07-04']);
+
