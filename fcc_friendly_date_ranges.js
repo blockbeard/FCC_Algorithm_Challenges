b3363a3c9c08ed67d3313a0c1ea21ae165e2ee06
Chris Wilson <chris2097@gmail.com>
2016-07-13 11:20:31 +0100

    Finished challenge.

--- fcc_friendly_date_ranges.js
+++ fcc_friendly_date_ranges.js
@@ -5,38 +5,121 @@
 function makeFriendlyDates(arr) {
     var start = new Date(arr[0]),
         finish = new Date(arr[1]),
+        current = new Date(),
         friendlyStart,
         friendlyEnd;
 
     //check if dates separated by less than 1 year
     function lessThanOneYear(){
-        console.log("Less than 1 year = "+(finish.getMilliseconds()- start.getMilliseconds()) <= 31556952000);
-    return ((finish.getMilliseconds()- start.getMilliseconds()) <= 31556952000);
+        console.log(finish.getTime() + " " + start.getTime());
+    return ((finish.getTime()- start.getTime()) < 31536000000);
 
     }
-    //check if date falls in this year
+
+    //check if date begins in this year
     function currentYear(){
+        console.log("currentYear() = " + (current.getFullYear() === start.getFullYear()));
+        return (current.getFullYear() === start.getFullYear());
     }
     //check if dates are in the same month
-    function sameMonth(){
+    function sameMonth() {
+        console.log("sameMonth() = " + (start.getMonth() === finish.getMonth()  &&  start.getFullYear() === finish.getFullYear()));
+        return (start.getMonth() === finish.getMonth()  &&  start.getFullYear() === finish.getFullYear());
     }
     //convert day into friendly
-    function friendlyDay(){
+    function friendlyDay(date){
+        var niceDay;
+        switch(date.getDate()){
+            case 1:
+                niceDay = "1st";
+                break;
+            case 2:
+                niceDay = "2nd";
+                break;
+            case 3:
+                niceDay = "3rd";
+                break;
+            default:
+                niceDay = date.getDate() + "th";
+        }
+        console.log("friendlyDay(date) = " + niceDay);
+        return niceDay;
     }
     //convert month into friendly
-    function friendlyMonth(){
+    function friendlyMonth(date){
+        var niceMonth;
+        switch (date.getMonth()){
+            case 0:
+                niceMonth = "January";
+                break;
+            case 1:
+                niceMonth = "February";
+                break;
+            case 2:
+                niceMonth = "March";
+                break;
+            case 3:
+                niceMonth = "April";
+                break;
+            case 4:
+                niceMonth = "May";
+                break;
+            case 5:
+                niceMonth = "June";
+                break;
+            case 6:
+                niceMonth = "July";
+                break;
+            case 7:
+                niceMonth = "August";
+                break;
+            case 8:
+                niceMonth = "September";
+                break;
+            case 9:
+                niceMonth = "October";
+                break;
+            case 10:
+                niceMonth = "November";
+                break;
+            case 11:
+                niceMonth = "December";
+                break;
+                        }
+                        console.log("friendlyMonth(date) = " + niceMonth);
+                        return niceMonth + " ";
     }
     //make start date into friendly
     function makeFriendlyStart(){
+        if (currentYear() && lessThanOneYear()){
+            friendlyStart = friendlyMonth(start) + friendlyDay(start);
+        }else{
+            friendlyStart = friendlyMonth(start) + friendlyDay(start) + ", " + start.getFullYear();
+        }
+        return friendlyStart;
     }
     //make end date into friendly
     function makeFriendlyEnd(){
+        if (sameMonth()) {
+            friendlyEnd = friendlyDay(finish);
+        }else if (lessThanOneYear()){
+            friendlyEnd = friendlyMonth(finish) + friendlyDay(finish);
+        }else{
+            friendlyEnd = friendlyMonth(finish) + friendlyDay(finish) + ", " + finish.getFullYear();
+        }
+        return friendlyEnd;
     }
-    lessThanOneYear();
-    console.log(friendlyStart + ", " + friendlyEnd);
-    return friendlyStart + ", " + friendlyEnd;
 
+    if ((start - finish) === 0){
+        console.log([makeFriendlyStart()]);
+        console.log(start + " " + finish);
+        return [makeFriendlyStart()];
+    }else {
+        console.log(start + " " + finish);
+        console.log([makeFriendlyStart(), makeFriendlyEnd()]);
+        return [makeFriendlyStart(), makeFriendlyEnd()];
+    }
 }
 
-makeFriendlyDates(['2016-07-01', '2016-07-04']);
+makeFriendlyDates(["2018-01-13", "2018-01-13"]);
 
