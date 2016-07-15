fb83743069a639a23b9a04127cea7ed6328eb59d
Chris Wilson <chris2097@gmail.com>
2016-07-15 21:12:53 +0100

    Challenge completed

--- fcc_map_the_debris.js
+++ fcc_map_the_debris.js
@@ -5,10 +5,15 @@
 function orbitalPeriod(arr) {
     var GM = 398600.4418;
     var earthRadius = 6367.4447;
-    var avgAlt = 378632.553;
+
     //formula
-    T = Math.round((2*Math.PI)*Math.sqrt((Math.pow(earthRadius + avgAlt, 3)/GM)));
-console.log(T);
+   // T = Math.round((2*Math.PI)*Math.sqrt((Math.pow(earthRadius + avgAlt, 3)/GM)));
+
+
+    for (var i = 0; i < arr.length; i++){
+        arr[i].orbitalPeriod =  Math.round((2*Math.PI)*Math.sqrt((Math.pow(earthRadius + arr[i].avgAlt, 3)/GM)));
+        delete arr[i].avgAlt;
+    }
     return arr;
 
 
